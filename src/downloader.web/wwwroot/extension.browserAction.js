22471b7fa4ae359e237ab6e189505b0086ead68e
jose.fajardo <fajardo_jf@hotmail.com>
2017-10-21 19:45:44 +1100

    got the full tab (dashboard) working   vs the "option"

---
+++ src/downloader.web/wwwroot/extension.browserAction.js
@@ -0,0 +1,22 @@
+﻿window.browser = (function () {
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome;
+})();
+var dashboardTabId = -1; // no id by default
+browser.browserAction.onClicked.addListener(function (activeTab) {
+    if (dashboardTabId === -1) {
+        browser.tabs.create({ url: 'ui/dashboard.html' }, function (tab) {
+        //browser.tabs.create({ url: 'https://login.aconex.com' }, function (tab) {
+            dashboardTabId = tab.id;
+        });
+    }
+    else {
+        browser.tabs.update(dashboardTabId, { "active": true, "selected": true });
+    }
+});
+browser.tabs.onRemoved.addListener(function (removetabid, removeInfo) {
+    if (dashboardTabId != -1 && removetabid === dashboardTabId) {
+        dashboardTabId = -1;
+    }
+});
